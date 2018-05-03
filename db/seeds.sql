d3b3e9222c3e8babbfce16871ee19313497150eb
knees <you@example.com>
2018-05-02 19:21:31 -0700

    new db, schema, server.js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("the_slammer", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("the_tobiyashi",false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("the_jenmeister", true);
