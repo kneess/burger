e8c63c7b2d7a5b9e308c423b1e391e15b16c1ff1
knees <you@example.com>
2018-05-08 17:56:44 -0700

    finished product finished up css and html

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -30,7 +30,7 @@ router.get("/", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
-  
+  console.log(req.body.devoured);
     burger.updateOne({
       devoured: req.body.devoured
     }, condition, function(result) {
--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("the_slammer", false);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("the_tobiyashi",false);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("the_jenmeister", false);
